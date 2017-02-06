bf35b39a363f7490401af0bd8ac805bd95c8d48a
braedenc <braedenchriske@gmail.com>
2017-02-06 12:24:20 -0600

    finishing remove method and starting test for iterator method

---
+++ CustomList/customList.cs
@@ -0,0 +1,35 @@
+﻿namespace CustomList
+{
+    public class customList<T>
+    {
+        public int Count = 1;
+        public T[] myArray = new T[1];
+        public void Add(T value)
+        {
+            T[] tempArray = new T[myArray.Length + 5];
+            for(int i = 0; i < myArray.Length; i++)
+            {
+
+                tempArray[i] = myArray[i];
+            }
+            Count++;
+            tempArray = myArray;
+        }
+
+        public bool Remove(T value)
+        {
+            if(Count > 0)
+            {
+                for(int i = this.Count; i > 0; i++)
+                {
+
+                }
+                Count--;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_To_List()
+        {
+            //Arrange
+            customList<int> list = new customList<int>();
+            int expectedLength = 1;
+            //Act
+            list.Add(15);
+            //Assert
+            Assert.AreEqual(list.Count ,expectedLength);
+        }
+
+        [TestMethod]
+        public void Remove_From_List()
+        {
+            //Arrange
+            customList<int> list = new customList<int>() { 3, 4};
+            int expectedLength = 1;
+            //Act
+            list.Remove(3);
+            //Assert
+            Assert.AreEqual(expectedLength, list.Count);
+        }
+    }
+}
