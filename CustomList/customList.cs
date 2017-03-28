bc5f262cb2b3a80981b38a16d7386b73a362f967
braedenc <braedenchriske@gmail.com>
2017-03-28 09:59:03 -0500

    resubmission

--- CustomList/customList.cs
+++ CustomList/customList.cs
@@ -4,36 +4,71 @@ using System;
 
 namespace CustomList
 {
-    public class customList<T> : IEnumerable
+    public class customList<T> : IEnumerable where T : IComparable
     {
-        public int Count = 1;
-        public T[] myArray = new T[1];
-        public void Add(T value)
-        {
-            T[] tempArray = new T[myArray.Length + 5];
-            for(int i = 0; i < myArray.Length; i++)
-            {
+        public int Count;
+        public int Capacity;
+        T[] myArray;
 
-                tempArray[i] = myArray[i];
-            }
-            Count++;
-            tempArray = myArray;
+        public T this[int i]
+        {
+            get { return myArray[i]; }
+            set { myArray[i] = value; }
+        }
+        public customList()
+        {
+            Count = 0;
+            Capacity = 5;
+            myArray = new T[Capacity];
         }
 
         public IEnumerator GetEnumerator()
         {
-            for(int i = 0; i < Count; i++)
+            for (int i = 0; i < myArray.Length; i++)
             {
                 yield return myArray[i];
             }
         }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void Add(T value)
+        {
+            if (Count >= Capacity)
+            {
+                T[] tempArray = new T[Capacity * 2];
+                for (int i = 0; i < Count; i++)
+                {
+
+                    tempArray[i] = myArray[i];
+                }
+                tempArray[Count] = value;
+                Count++;
+                myArray = tempArray;
+            }
+            else
+            {
+                T[] tempArray = new T[Capacity];
+                for (int i = 0; i < Count; i++)
+                {
+
+                    tempArray[i] = myArray[i];
+                }
+                tempArray[Count] = value;
+                Count++;
+                myArray = tempArray;
+            }
+
+        }
+
         public void Remove(T value)
         {
             T[] tempArray = new T[myArray.Length];
-            for(int i = 0; i > this.Count; i++)
+            for (int i = 0; i > this.Count; i++)
             {
-                if(myArray[i].Equals(value))
+                if (myArray[i].Equals(value))
                 {
                     myArray[i] = default(T);
                 }
@@ -44,6 +79,100 @@ namespace CustomList
             }
             myArray = tempArray;
             Count--;
+        }
+
+        public override string ToString()
+        {
+            string convertedItems = "";
+            foreach (T item in myArray)
+            {
+                if (convertedItems == "")
+                {
+                    convertedItems += item.ToString();
+                }
+                else
+                {
+                    convertedItems += string.Format(", {0}", item);
+                }
+            }
+            return convertedItems;
+        }
+
+        public static customList<T> operator +(customList<T> firstList, customList<T> secondList)
+        {
+            T[] tempArray = new T[firstList.Capacity + secondList.Capacity];
+            customList<T> addedList = new customList<T>();
+            addedList.myArray = tempArray;
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                addedList.Add(firstList[i]);
+            }
+            for (int i = addedList.Count; i < firstList.Count + secondList.Count; i++)
+            {
+                addedList.Add(secondList[i - firstList.Count]);
+            }
+            return addedList;
+        }
+        public static customList<T> operator -(customList<T> firstList, customList<T> secondList)
+        {
+            T[] tempArray = new T[firstList.Capacity];
+            customList<T> subtractedList = new customList<T>();
+            subtractedList.myArray = tempArray;
+
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                subtractedList.Add(firstList[i]);
+            }
+            for (int i = 0; i < secondList.Count; i++)
+            {
+                if (subtractedList[i].Equals(secondList[i]))
+                {
+                    subtractedList.Remove(secondList[i]);
+                }
+            }
+            return subtractedList;
+        }
+        public customList<T> Zip(customList<T> secondList)
+        {
+            T[] tempArray = new T[this.Capacity + secondList.Capacity];
+            customList<T> zippedList = new customList<T>();
+            zippedList.myArray = tempArray;
+
+            if (this.Count > secondList.Count)
+            {
+                for (int i = 0; i < secondList.Count; i++)
+                {
+                    zippedList.Add(this[i]);
+                    zippedList.Add(secondList[i]);
+                }
+                for (int i = secondList.Count; i < this.Count; i++)
+                {
+                    zippedList.Add(this[i]);
+                }
+            }
+            else if (this.Count < secondList.Count)
+            {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    zippedList.Add(this[i]);
+                    zippedList.Add(secondList[i]);
+                }
+                for (int i = this.Count; i < secondList.Count; i++)
+                {
+                    zippedList.Add(secondList[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < this.Count; i++)
+                {
+                    zippedList.Add(this[i]);
+                    zippedList.Add(secondList[i]);
+                }
+            }
+
+            return zippedList;
         }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -16,14 +16,14 @@ namespace UnitTestProject1
             //Act
             list.Add(15);
             //Assert
-            Assert.AreEqual(list.Count ,expectedLength);
+            Assert.AreEqual(list.Count, expectedLength);
         }
 
         [TestMethod]
         public void Remove_From_List()
         {
             //Arrange
-            customList<int> list = new customList<int>() { 3, 4};
+            customList<int> list = new customList<int>() { 3, 4 };
             int expectedLength = 1;
             //Act
             list.Remove(3);
@@ -36,11 +36,77 @@ namespace UnitTestProject1
         {
             //Arrange
             customList<int> list = new customList<int>();
-            int[] numbers;
             //Act
-            list.GetEnumerator();
+            for (int i = 0; i < 2; i++)
+            {
+                list.Add(i);
+            }
             //Assert
-            
+            Assert.AreEqual(2, list.Count);
+
+        }
+
+        [TestMethod]
+        public void ToString()
+        {
+            //Arrange
+            customList<int> list = new customList<int>();
+            //Act
+            for (int i = 0; i < 2; i++)
+            {
+                list.Add(i);
+            }
+            var strings = list.ToString();
+            //Assert
+            Assert.AreEqual("0, 1, 0, 0, 0", strings);
+        }
+
+        [TestMethod]
+        public void OverloadPlus()
+        {
+            //Arrange
+            customList<int> list1 = new customList<int>() { 1, 2 };
+            customList<int> list2 = new customList<int>() { 3, 4 };
+            customList<int> test = new customList<int>() { 1, 2, 3, 4 };
+            //Act
+            var addedLists = list1 + list2;
+            //Assert
+            Assert.AreEqual(addedLists[3], test[3]);
+        }
+
+        [TestMethod]
+        public void OverloadMinus()
+        {
+            //Arrange
+            customList<int> list1 = new customList<int>() { 1, 2, 5, 3, 4 };
+            customList<int> list2 = new customList<int>() { 5, 5, 5, 5, 5 };
+            //Act
+            var subtractedLists = list1 - list2;
+            //Assert
+            Assert.AreNotEqual(subtractedLists[2], 5);
+        }
+
+        [TestMethod]
+        public void Zip()
+        {
+            //Arrange
+            customList<int> list1 = new customList<int>();
+            customList<int> list2 = new customList<int>();
+            customList<int> test = new customList<int>() { 1, 2, 3, 4, 5 };
+            //Act
+            list1.Add(1);
+            list1.Add(3);
+            list1.Add(5);
+            list2.Add(2);
+            list2.Add(4);
+            var zippedList = list1.Zip(list2);
+            //Assert
+            Assert.AreEqual(zippedList[0], test[0]);
+            Assert.AreEqual(zippedList[1], test[1]);
+            Assert.AreEqual(zippedList[2], test[2]);
+            Assert.AreEqual(zippedList[3], test[3]);
+            Assert.AreEqual(zippedList[4], test[4]);
         }
     }
 }
+
